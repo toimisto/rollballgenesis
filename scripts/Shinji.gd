e28ac72cdbcb5505999c18db8d8b6ab577be947c
Glukoosi <aleksi@seppanen.dev>
2019-10-05 14:26:07 +0300

    Change ball physics

--- scripts/Shinji.gd
+++ scripts/Shinji.gd
@@ -1,34 +1,13 @@
-extends KinematicBody
+extends RigidBody
 
-var velocity = Vector3(0,0,0)
-const SPEED = 6
-const ROTSPEED = 7
+const movementSpeed = 25
 
 func _ready():
 	pass
 
 func _physics_process(delta):
+	var xSpeed = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	var zSpeed = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	var movementVec = Vector3(xSpeed, 0, zSpeed) * movementSpeed
 	
-	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
-		velocity.x = 0
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
-		$MeshInstance.rotate_z(deg2rad(-ROTSPEED))
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
-		$MeshInstance.rotate_z(deg2rad(ROTSPEED))
-	else:
-		velocity.x = lerp(velocity.x,0,0.1)
-
-	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
-		velocity.z = 0
-	elif Input.is_action_pressed("ui_up"):
-		velocity.z = -SPEED
-		$MeshInstance.rotate_x(deg2rad(-ROTSPEED))
-	elif Input.is_action_pressed("ui_down"):
-		velocity.z = SPEED
-		$MeshInstance.rotate_z(deg2rad(ROTSPEED))
-	else:
-		velocity.z = lerp(velocity.z,0,0.1)
-		
-	move_and_slide(velocity)
+	add_central_force(movementVec)
