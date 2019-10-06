2024aee11a49166e3e9c3b1fa029b4ab7d2e9282
Glukoosi <aleksi@seppanen.dev>
2019-10-06 11:28:08 +0300

    Add 2D mode

--- Level.gd
+++ Level.gd
@@ -3,15 +3,21 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var DIM = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    SelectCamera(DIM)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #    pass
 
+func SelectCamera(dimension):
+    if dimension == 2:
+        $Camera2D.make_current()
+    elif dimension == 3:
+        $Shinji/Camera.make_current()
 
 func _on_Control_killshinji():
     if $Floor:
--- scripts/Shinji.gd
+++ scripts/Shinji.gd
@@ -8,6 +8,10 @@ func _ready():
 func _physics_process(delta):
     var xSpeed = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
     var zSpeed = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+    
+    if get_parent().DIM == 2:
+        set_axis_lock(4, 1)
+    
     var movementVec = Vector3(xSpeed, 0, zSpeed) * movementSpeed
 
     add_central_force(movementVec)
