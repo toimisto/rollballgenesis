2eaf03d03d0794fb5223dcb03d35ad34ba9689b5
Glukoosi <aleksi@seppanen.dev>
2019-10-07 14:05:15 +0300

    Fix color script

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -43,26 +43,36 @@ func toggle_object(object):
 
 func toggle_color(color):
     if color:
-        $Floor/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(0,0.8,0,1)
-        $Walls/wall/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(0.7,0,0,1)
-        $Box/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(0.67,0.54,0.18,1)
-        $Shinji/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(.5,0,1,1)
-        $Goal/Flag1.mesh.surface_get_material(0).albedo_color = Color(1,0,1,1)
-        $Goal/Flag2.mesh.surface_get_material(0).albedo_color = Color(1,0,1,1)
-        $Goal/Flag3.mesh.surface_get_material(0).albedo_color = Color(1,0,1,1)
-        $Goal/Flag4.mesh.surface_get_material(0).albedo_color = Color(1,0,1,1)
-        $Goal/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(.2,.2,.2,1)
+        if $Floor:
+            $Floor/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(0,0.8,0,1)
+        if $Walls:
+            $Walls/wall/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(0.7,0,0,1)
+        if $Box:
+            $Box/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(0.67,0.54,0.18,1)
+        if $Shinji:
+            $Shinji/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(.5,0,1,1)
+        if $Goal:
+            $Goal/Flag1.mesh.surface_get_material(0).albedo_color = Color(1,0,1,1)
+            $Goal/Flag2.mesh.surface_get_material(0).albedo_color = Color(1,0,1,1)
+            $Goal/Flag3.mesh.surface_get_material(0).albedo_color = Color(1,0,1,1)
+            $Goal/Flag4.mesh.surface_get_material(0).albedo_color = Color(1,0,1,1)
+            $Goal/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(.2,.2,.2,1)
         $WorldEnvironment.environment.background_color = Color(.6,.6,1,1)
     else:
-        $Floor/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
-        $Walls/wall/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
-        $Box/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
-        $Shinji/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
-        $Goal/Flag1.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
-        $Goal/Flag2.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
-        $Goal/Flag3.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
-        $Goal/Flag4.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
-        $Goal/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
+        if $Floor:
+            $Floor/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
+        if $Walls:
+            $Walls/wall/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
+        if $Box:
+            $Box/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
+        if $Shinji:
+            $Shinji/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
+        if $Goal:
+            $Goal/Flag1.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
+            $Goal/Flag2.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
+            $Goal/Flag3.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
+            $Goal/Flag4.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
+            $Goal/MeshInstance.mesh.surface_get_material(0).albedo_color = Color(1,1,1,1)
         $WorldEnvironment.environment.background_color = Color(1,1,1,1)
 
 func toggle_gravity(gravity):
